350dd049718614ad22ee71ab18f550daecd1189f
mizuki <milky_29@docomo.ne.jp>
2024-08-22 12:18:26 +0900

    changing the reching error fig

--- code/reaching_error_plt.py
+++ code/reaching_error_plt.py
@@ -15,11 +15,18 @@ plt.figure(figsize=(10,6))
 plt.plot(df['Time'], df['ms_left_model'], label='Estimation by Model', color='red')
 plt.plot(df['Time'], df['ms_left_sensor_estimate'], label='Estimation by Fiber Sensor', color='blue')
 plt.plot(df['Time'], df['true_encoder'], label='Linear Encoder', color='green')
+
+# 2.5秒の時点に黒の点線を追加
+plt.axvline(x=2.7, color='black', linestyle='--', linewidth=1)
+# テキストを追加
+plt.text(6.1, 157, 'Strain Gauge Loses Tension', verticalalignment='center', horizontalalignment='right', fontsize=11, color='black')
+# 横軸に2.7のラベルを追加
+plt.xticks(list(plt.xticks()[0]) + [2.7])  # 現在のラベルに2.7を追加
 # グラフの装飾
 plt.xlabel('Time [s]')
 plt.ylabel('Length [mm]')
 plt.xlim(0, 12)  # 横軸の範囲を0から1200/100に制限
-plt.ylim(135,170)
+plt.ylim(130,165)
 plt.legend(loc='upper right',fontsize=14)
 
 # グラフを表示
